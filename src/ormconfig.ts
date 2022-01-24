f3a8385bef53f290272cbb9cd73eaa26f4ddaba8
Gleb <unicode256@yandex.by>
2022-01-25 00:50:16 +0300

    fix ssl rejection #2

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -9,8 +9,7 @@ const config: ConnectionOptions = {
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-  extra: {
-    ssl: true,
+  ssl: {
     rejectUnauthorized: false,
   },
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
